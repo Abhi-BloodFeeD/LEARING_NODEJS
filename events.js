624ae072dc404a348db546ccb4935729d25639b4
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2021-04-23 17:10:19 +0530

    UNDERSTANDING SOME MORE CONCEPTS

--- events.js
+++ events.js
@@ -1,10 +1,10 @@
 const EventEmitter = require('events')
 //EventEmitter is a class
 const customEmitter = new EventEmitter()
-  
+
 //when the event 'response' takes place
 customEmitter.on('response',() => {
   console.log(`data received`)
 })
 //emit function
-customEmitter.emit(`response`)  
+customEmitter.emit(`response`)
--- learningSync.js
+++ learningSync.js
@@ -2,7 +2,7 @@ const {readFile} = require('fs')
 const util = require('util')
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
-
+ 
 const start = async() =>{
   try{
     const first = await readFilePromise(`./content/first.txt`,`utf8`)
