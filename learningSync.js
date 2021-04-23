21bf5f004ab0c481ff001000d6c0637d69736488
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2021-04-23 14:48:06 +0530

    AWAIT ASYNC PROMISES

--- learningSync.js
+++ learningSync.js
@@ -1,4 +1,4 @@
-const {readFile,writeFile} = require('fs')
+const {readFile} = require('fs')
 const util = require('util')
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
