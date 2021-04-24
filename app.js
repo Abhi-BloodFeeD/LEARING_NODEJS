3cb9c5b9cf8bdd4d76d272bd68b05028b93ad172
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2021-04-25 00:31:23 +0530

    HTTP MODULE

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const EventEmitter = require('events')
-//EventEmitter is a class
-const customEmitter = new EventEmitter()
+
+  const http = require('http')
+  const {readFileSync} = require('fs');
   
-//when the event 'response' takes place
-customEmitter.on('response',() => {
-  console.log(`data received`)
-})
-//emit function
-customEmitter.emit(`response`)  
+  const homePage = readFileSync('./index.html')
+
+  const server = http.createServer((req,res)=>{
+    const url = req.url
+    //homepage
+    if(url ==='/'){
+      res.writeHead(200,{'content-type':'text/html'})
+      res.write(homePage)
+      res.end()
+    }
+    else if(url ===`/about`){
+      res.writeHead(200,{'content-type':'text/html'})
+      res.write(`<h1>home page</h1>`)
+      res.end()
+    }
+    else{
+      res.writeHead(404,{'content-type':'text/html'})
+      res.write(`<h1>404 PAGE NOT FOUND</h1>`)
+      res.end()
+    }
+  })
+  server.listen(5000);
--- httpshare.js
+++ httpshare.js
@@ -1,1 +1,8 @@
+  const http = require('http')
   
+  const server = http.createServer((req,res)=>{
+    console.log('USER HERE')
+    res.end()
+  }) 
+  
+  server.listen(5000)
